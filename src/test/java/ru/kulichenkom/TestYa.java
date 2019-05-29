33a3dce90310737a7252fbe2c272c72ec213786c
Maxim <kulichenkom@ya.ru>
2019-05-30 00:02:31 +0300

    убрал цикл

--- src/test/java/ru/kulichenkom/TestYa.java
+++ src/test/java/ru/kulichenkom/TestYa.java
@@ -3,7 +3,6 @@ package ru.kulichenkom;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -13,19 +12,12 @@ public class TestYa {
     @Test
     public void openYa() {
         open("http://yandex.ru ");
-        for (int i = 0; i < 10; i++) {//
-            if ($(".input__control").exists()) {
-                $(".input__control").setValue(searchingText);
-            } else
-                $("input#text").setValue(searchingText);
-            if ($(".websearch-button__text").exists()) {
-                $(".websearch-button__text").click();
-            } else
-                $("div.search2__button").click();
-            if ($(".card__narrow").exists()) {
-                $$(".typo_text_l").shouldHave(size(13));
-            } else $$(".typo_text_l").shouldHave(size(11));
-            $(".typo_text_l").shouldHave(Condition.text(searchingText));
-        }
+        $("input#text").setValue(searchingText);
+        $("div.search2__button").click();
+        if ($(".card__narrow").exists()) {
+            $$(".typo_text_l").shouldHave(size(13));
+        } else $$(".typo_text_l").shouldHave(size(11));
+        $(".typo_text_l").shouldHave(Condition.text(searchingText));
     }
 }
+
